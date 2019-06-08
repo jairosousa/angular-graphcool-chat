327368250e3e890c54ed6970739c8cf39a43b928
Jairo <jaironsousa@gmail.com>
2019-06-08 19:37:56 -0300

    26 - Query na API GraphQL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { query } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-graphcool-chat';
+  
+  private appUrl = "https://api.graph.cool/simple/v1/cjwmsgavv3gjd016945kj2dga";
+
+  constructor(
+    private http: HttpClient
+  ){
+    this.allUsers();
+  }
+
+  allUsers():void {
+    const body = {
+      query: `
+        query{
+          allUsers{
+            id
+            name
+            email
+          }
+        }
+      `
+    }
+
+    this.http.post(this.appUrl, body)
+      .subscribe(res => console.log('Query: ', res));
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import {MatToolbarModule, MatListModule} from '@angular/material';
+import { MatToolbarModule, MatListModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,11 +10,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  ], 
   imports: [
-    BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatListModule
   ],
